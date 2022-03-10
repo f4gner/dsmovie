9597a397dcacdf7001da729727037acc6ca723e7
Fagner dias <fagnersquadrao@gmail.com>
2022-03-10 20:07:53 -0300

    correçoes front-end 3x

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -14,13 +14,13 @@ function MovieCard() {
 
     return (
         <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
 
 
-                <Link to={"/form/${movie.id}"}>
+                <Link to={`/for/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>  
                 </Link>
                 
